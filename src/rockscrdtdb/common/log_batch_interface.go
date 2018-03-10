9c700c8c777a6eae5744747a3f43f2b5db44f8b2
preguica <nmp@fct.unl.pt>
2018-03-10 17:02:43 +0100

    changed .ignore

--- src/rockscrdtdb/common/log_batch_interface.go
+++ src/rockscrdtdb/common/log_batch_interface.go
@@ -1,3 +1,7 @@
+// Copyright 2018 Nuno Preguica, NOVA LINCS, FCT, Universidade NOVA de Lisboa.
+// All rights reserved.
+// Use of this source code is governed by Apache 2.0
+// license that can be found in the LICENSE file.
 package common
 
 type LogBatchInterface interface {
